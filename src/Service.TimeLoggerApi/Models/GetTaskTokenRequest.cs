4c087cbc9c4af30ed0732a1b909b5f3178a63d2b
dmitriy.b <dmitriy.b@smplt.net>
2022-02-07 09:52:27 +0500

    Add validation to controller model

--- src/Service.TimeLoggerApi/Models/GetTaskTokenRequest.cs
+++ src/Service.TimeLoggerApi/Models/GetTaskTokenRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Service.Core.Client.Education;
 
 namespace Service.TimeLoggerApi.Models
@@ -6,12 +7,18 @@ namespace Service.TimeLoggerApi.Models
 	public class GetTaskTokenRequest
 	{
 		[Required]
+		[Range(1, 9)]
+		[DefaultValue(EducationTutorial.PersonalFinance)]
 		public EducationTutorial Tutorial { get; set; }
 
 		[Required]
+		[Range(1, 5)]
+		[DefaultValue(1)]
 		public int Unit { get; set; }
 
 		[Required]
+		[Range(1, 6)]
+		[DefaultValue(1)]
 		public int Task { get; set; }
 	}
 }
